4eaf50c6619740f65fed14ae2522a26bfcf8836c
Marko Milovanovic <Marko Milovanovic>
2016-10-22 13:53:57 -0400

    Updated Sign Up Page

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 var main = function() {
-  /* Push the body and the nav over by 285px over */
+
   $('.sign-up-btn').click(function() {
     $('.login-form').fadeOut(1000);
-    $('.sign-up-form').fadeIn(3000);
+    $('.sign-up-form').delay(800).fadeIn(3000);
   });
 
+$('.btn-2').click(function() {
+    $('.sign-up-form').hide(1000);
+    $('.login-form').fadeIn(3000);
+  });
 
 };
 
