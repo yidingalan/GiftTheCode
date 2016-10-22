40637045ddaedf5a904c18889e95be85b74c77a0
Jackson Guo <jackson.guo@horizn.com>
2016-10-22 16:17:18 -0400

    adding login/signup validation

---
+++ loginValidation.php
@@ -0,0 +1,15 @@
+<?php
+
+/*
+
+Login and sign up form validation.
+Note: Valid users hardcoded at the moment.
+
+*/
+
+//print_r($_SERVER);
+print_r($_POST);
+
+
+
+?>
---
+++ signupValidation.php
@@ -0,0 +1,14 @@
+<?php
+
+/*
+
+Login and sign up form validation.
+Note: Valid users hardcoded at the moment.
+
+*/
+
+//print_r($_SERVER);
+print_r($_POST);
+header('Location:index.html');
+
+?>
